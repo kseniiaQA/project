1d611185c3eae5b1c5f49f9b8fbfc9823a56e67d
Your Name <you@example.com>
2021-06-08 14:15:16 +0300

    Revert commit

--- src/test/java/ru/netology/StatstServiceTest.java
+++ src/test/java/ru/netology/StatstServiceTest.java
@@ -68,7 +68,7 @@ public class StatstServiceTest {
         int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int expected = 5;
 
-int middle=15;
+        int middle=15;
 
         int actual = service.findLessThanMiddle(sales, middle);
         assertEquals(expected, actual);
